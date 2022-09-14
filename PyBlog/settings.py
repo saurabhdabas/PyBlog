eaf726993e5be54b09c3fd83ed986863a8473bba
Saurabh Dabas <developerdabas@gmail.com>
2022-09-14 00:00:18 -0600

    Add setting for Media URl

--- PyBlog/settings.py
+++ PyBlog/settings.py
@@ -116,6 +116,9 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets/'),
 )
+
+MEDIA_URL= '/media/'
+MEDIA__ROOT=os.path.join(BASE_DIR,'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
