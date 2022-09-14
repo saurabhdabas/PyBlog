969b8e9e547fb15b3528c427c6c03e98b5d4f807
Saurabh Dabas <developerdabas@gmail.com>
2022-09-13 21:51:39 -0600

    Add path for each post

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-  path('', views.posts, name='posts'),
+  path('posts/', views.posts, name='posts'),
+  path('posts/<slug>', views.post, name='post'),
 ]
