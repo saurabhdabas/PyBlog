1edbeb9828fcb90e81b056543330237b213f537b
Saurabh Dabas <developerdabas@gmail.com>
2022-09-13 21:52:11 -0600

    Add new function for post

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def posts(request):
   posts = Post.objects.all().order_by('date')
   return render(request,'posts/posts.html',{'posts':posts})
+
+def post(request,slug):
+  post = Post.objects.filter(slug = slug)
+  return render(request,'posts/post.html',{'post':post})
